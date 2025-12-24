77049960383adfd0256197ea54fa133d2ba697ad
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-12-24 17:50:03 +0100

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -10,13 +10,15 @@ func SetupRoutes(router *gin.Engine) {
 	uploadController := controllers.GetUploadController()
 
 	router.GET("/health", uploadController.HealthCheck)
+	router.POST("/upload", uploadController.UploadFile)
+	router.POST("/delete", uploadController.DeleteFile)
 
-	api := router.Group("/api")
-	{
-		upload := api.Group("/upload")
-		{
-			upload.POST("/", uploadController.UploadFile)
-			upload.DELETE("/", uploadController.DeleteFile)
-		}
-	}
+	// api := router.Group("/api")
+	// {
+	// 	upload := api.Group("/upload")
+	// 	{
+	// 		upload.POST("", uploadController.UploadFile)
+	// 		upload.POST("/delete", uploadController.DeleteFile)
+	// 	}
+	// }
 }
