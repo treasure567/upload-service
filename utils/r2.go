aeca2c2dc1ef7956f2ff3a5ad4133ae9b153e90c
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-12-24 07:53:08 +0100

    Create r2.go

---
+++ utils/r2.go
@@ -0,0 +1,98 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"go-upload/config"
+	"go-upload/models"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/google/uuid"
+)
+
+func UploadToR2(fileBuffer []byte, folderPath string, ext string) (*models.UploadResult, error) {
+	cfg := config.AppConfig.R2
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(cfg.Region),
+		Endpoint:    aws.String(cfg.Endpoint),
+		Credentials: credentials.NewStaticCredentials(cfg.AccessKeyID, cfg.SecretAccessKey, ""),
+		S3ForcePathStyle: aws.Bool(true),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create session: %w", err)
+	}
+
+	svc := s3.New(sess)
+
+	filename := fmt.Sprintf("%s.%s", uuid.New().String(), ext)
+	objectKey := filepath.Join(folderPath, filename)
+
+	contentType := getContentType(ext)
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(cfg.BucketName),
+		Key:         aws.String(objectKey),
+		Body:        bytes.NewReader(fileBuffer),
+		ContentType: aws.String(contentType),
+		ACL:         aws.String("public-read"),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to upload to R2: %w", err)
+	}
+
+	publicURL := fmt.Sprintf("%s/%s", cfg.CustomDomain, objectKey)
+
+	return &models.UploadResult{
+		URL:      publicURL,
+		PublicID: objectKey,
+	}, nil
+}
+
+func DeleteFromR2(key string) error {
+	cfg := config.AppConfig.R2
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(cfg.Region),
+		Endpoint:    aws.String(cfg.Endpoint),
+		Credentials: credentials.NewStaticCredentials(cfg.AccessKeyID, cfg.SecretAccessKey, ""),
+		S3ForcePathStyle: aws.Bool(true),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create session: %w", err)
+	}
+
+	svc := s3.New(sess)
+
+	_, err = svc.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(cfg.BucketName),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete from R2: %w", err)
+	}
+
+	return nil
+}
+
+func getContentType(ext string) string {
+	contentTypes := map[string]string{
+		"jpg":  "image/jpeg",
+		"jpeg": "image/jpeg",
+		"png":  "image/png",
+		"gif":  "image/gif",
+		"pdf":  "application/pdf",
+		"mp4":  "video/mp4",
+		"mov":  "video/quicktime",
+		"zip":  "application/zip",
+	}
+
+	if ct, ok := contentTypes[ext]; ok {
+		return ct
+	}
+	return "application/octet-stream"
+}
